f661bc38a3b7128f9c4af21ba0ffc6a36ca3321a
serhatprogramming <serhatprogramming@gmail.com>
2023-05-04 12:03:01 -0400

    list of books fixed.

--- index.js
+++ index.js
@@ -94,7 +94,9 @@ const resolvers = {
     bookCount: () => Book.collection.countDocuments(),
     authorCount: () => Author.collection.countDocuments(),
     allBooks: async (root, args) => {
+      console.log("check 1");
       let books = await Book.find({}).populate("author");
+      console.log("check 2");
       books = args.author
         ? books.filter((book) => book.author.name === args.author)
         : books;
